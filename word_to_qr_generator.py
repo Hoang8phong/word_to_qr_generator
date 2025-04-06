37859ad033ae112dd6f864fd7c5a08ea9179a3cb
Brian Hoang <132343806+Hoang8phong@users.noreply.github.com>
2025-04-06 22:18:23 +1000

    Update word_to_qr_generator.py

--- word_to_qr_generator.py
+++ word_to_qr_generator.py
@@ -1,5 +1,7 @@
 import qrcode
 from qrcode.image.pure import PyPNGImage
+import tkinter as tk
+from tkinter import colorchooser
 
 
 data = input("Enter the text or URL: ").strip()
@@ -8,13 +10,24 @@ filename = input("Enter the file name: ").strip()
 if not filename.endswith('.png'):
     filename += '.png'
 
+
 qr = qrcode.QRCode(box_size=10, border=4)
 qr.add_data(data)
 
-image = qr.make_image(fill_color = 'black', back_color = 'white', image_factory=PyPNGImage)
+qr.make(fit=True)
+
+
+root = tk.Tk() #Create main window
+root.withdraw() #Hide window right after choose
+
+fill_color = colorchooser.askcolor(title = "Choose color")[1]
+back_color = colorchooser.askcolor(title = "Choose color")[1]
+
+image = qr.make_image(fill_color = fill_color, back_color = back_color, image_factory=PyPNGImage)
 image.save(filename)
 
 print(f'QR code saved as {filename}')
+
 
 
 
